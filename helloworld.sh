0b169396fecada575875b8584cae96f7249232d5
Eric Santos <djericsantos1@gmail.com>
2022-11-10 18:22:36 -0800

    Add files via upload

---
+++ helloworld.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+#script: Ops 201 Class 02 Ops Challenge Solution
+#Author: Eric Santos
+#Date of latest Revision: 11/10/2022
+#Purpose: Print a string to the terminal
+echo "I am working perfectly"
+# 	Main
+# 	End
